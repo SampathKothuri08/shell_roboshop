db8cc2ce8c85e74dcb7fef67eeed5ea1ba50dbd7
sampath <sampathsvrk@gmail.coml>
2025-07-04 18:11:58 -0400

    Added mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -55,7 +55,7 @@ systemctl start mongod &>> $LOG_FILE
 
 Validate $? "Starting mongodb"
 
-sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>> $LOG_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
 Validate $? "Giving access to the remote connections"
 
